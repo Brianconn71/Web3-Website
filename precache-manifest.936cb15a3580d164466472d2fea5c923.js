1dc60b8761dd0b885f0a7b08ae3d1e1cad385146
Brian Connolly <Brian.connolly71@gmail.com>
2021-10-16 17:23:36 +0100

    Updates

---
+++ precache-manifest.936cb15a3580d164466472d2fea5c923.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "14c12aa7465cdb3f0879",
+    "url": "/Web3-Website/static/css/main.5cc84802.chunk.css"
+  },
+  {
+    "revision": "14c12aa7465cdb3f0879",
+    "url": "/Web3-Website/static/js/main.14c12aa7.chunk.js"
+  },
+  {
+    "revision": "1a58e9a2e7e7dfe19075",
+    "url": "/Web3-Website/static/css/1.e3d20510.chunk.css"
+  },
+  {
+    "revision": "1a58e9a2e7e7dfe19075",
+    "url": "/Web3-Website/static/js/1.1a58e9a2.chunk.js"
+  },
+  {
+    "revision": "5b0297ed92940224cde1",
+    "url": "/Web3-Website/static/js/runtime~main.5b0297ed.js"
+  },
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/Web3-Website/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "7ef345dba64d0af0586508247643415e",
+    "url": "/Web3-Website/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Web3-Website/precache-manifest.936cb15a3580d164466472d2fea5c923.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Web3-Website/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
